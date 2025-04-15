94a6346516f4cba58f21cc5393bdf3e7704e781f
AJ Ruf <z0isch@gmail.com>
2025-04-14 23:35:48 -0400

    gh page

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/vibe-bombparty/",
   server: {
     port: 5173,
     host: true,
